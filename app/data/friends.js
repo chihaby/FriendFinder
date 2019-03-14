b6372e2d22dc41f06c3eb37c58087f74889a2afe
Radouane <38984550+Radouane@users.noreply.github.com>
2019-03-14 09:31:08 -0700

    second update'

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,38 +1,44 @@
 var friendsData = [{
 
-    "name": "Ed",
-    "photo": "./images/ed.jpg", 
+    firstName: "Ed",
+    lastName: "thompson",
+    photo: "./images/ed.jpg", 
     "scores": [4,5,3,3,2,4,5,1,2,5,3]
     },
 
     {
-    "name": "Linda",
-    "photo": "./images/linda.jpg",
-    "scores": [2,4,3,5,1,2,3,2,4,1]
+    firstName: "Linda",
+    lastName: "gerrard",
+    photo: "./images/linda.jpg",
+    scores: [2,4,3,5,1,2,3,2,4,1]
     },
 
     {
-    "name": "Eric",
-    "photo": "./images/eric.jpg", 
-    "scores": [3,4,5,1,2,2,1,5,2,4]
+    firstName: "Eric",
+    lastName: "baldwin",
+    photo: "./images/eric.jpg", 
+    scores: [3,4,5,1,2,2,1,5,2,4]
     },
 
     {
-    "name": "Patricia",
-    "photo": "./images/patricia.jpg",
-    "scores": [2,2,2,5,5,4,4,1,3,2]
+    firstName: "Patricia",
+    lastName: "cass",
+    photo: "./images/patricia.jpg",
+    scores: [2,2,2,5,5,4,4,1,3,2]
     },
 
     {
-    "name": "Olivia",
-    "photo": "./images/olivia.jpg",
-    "scores": [3,3,3,3,5,2,1,3,2,4]
+    firstName: "Olivia",
+    lastName: "Ed",
+    photo: "./images/olivia.jpg",
+    scores: [3,3,3,3,5,2,1,3,2,4]
     },
 
     {
-    "name": "Raul",
-    "photo": "./images/raul.jpg",
-    "scores": [4,4,4,1,2,1,5,4,2,3]
+    firstName: "Raul",
+    lastName: "de blassio",
+    photo: "./images/raul.jpg",
+    scores: [4,4,4,1,2,1,5,4,2,3]
     }
 ];
 
