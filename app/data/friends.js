c314d0412b92c3913fdf3b6b9c23a305af4a6951
Radouane <38984550+Radouane@users.noreply.github.com>
2019-03-14 08:30:22 -0700

    update

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friendsData = [{
+
+    "name": "Ed",
+    "photo": "./images/ed.jpg", 
+    "scores": [4,5,3,3,2,4,5,1,2,5,3]
+    },
+
+    {
+    "name": "Linda",
+    "photo": "./images/linda.jpg",
+    "scores": [2,4,3,5,1,2,3,2,4,1]
+    },
+
+    {
+    "name": "Eric",
+    "photo": "./images/eric.jpg", 
+    "scores": [3,4,5,1,2,2,1,5,2,4]
+    },
+
+    {
+    "name": "Patricia",
+    "photo": "./images/patricia.jpg",
+    "scores": [2,2,2,5,5,4,4,1,3,2]
+    },
+
+    {
+    "name": "Olivia",
+    "photo": "./images/olivia.jpg",
+    "scores": [3,3,3,3,5,2,1,3,2,4]
+    },
+
+    {
+    "name": "Raul",
+    "photo": "./images/raul.jpg",
+    "scores": [4,4,4,1,2,1,5,4,2,3]
+    }
+];
+
+module.exports = friendsData;
---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,42 @@
+var friendsData = require("../data/friends");
+
+module.exports = function(app) {
+    
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsData);
+    });
+
+
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out survey request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+
+    app.post("/api/friends", function(req, res) {
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        // req.body is available since we're using the body parsing middleware
+        if (friendsData) {
+            friendsData.push(req.body);
+            res.json(true);
+        }
+        res.json(false);    
+    });
+
+    // I added this below code to clear out array while working with the functionality.
+
+    app.post("/api/clear", function(req, res) {
+        tableData.length = [];
+        res.json({ ok: true });
+    });
+};
