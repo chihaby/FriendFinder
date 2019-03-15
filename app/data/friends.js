a63130142b3222edcbd0793f02d73c536415df1d
Radouane <38984550+Radouane@users.noreply.github.com>
2019-03-15 10:35:45 -0700

    'update'

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,43 +2,43 @@ var friendsData = [{
 
     firstName: "Ed",
     lastName: "thompson",
-    photo: "./images/ed.jpg", 
-    "scores": [4,5,3,3,2,4,5,1,2,5,3]
+    //photo: "./images/ed.jpg", 
+    //"scores": [4,5,3,3,2,4,5,1,2,5,3]
     },
 
     {
     firstName: "Linda",
     lastName: "gerrard",
-    photo: "./images/linda.jpg",
-    scores: [2,4,3,5,1,2,3,2,4,1]
+    //photo: "./images/linda.jpg",
+    //scores: [2,4,3,5,1,2,3,2,4,1]
     },
 
     {
     firstName: "Eric",
     lastName: "baldwin",
-    photo: "./images/eric.jpg", 
-    scores: [3,4,5,1,2,2,1,5,2,4]
+    //photo: "./images/eric.jpg", 
+    //scores: [3,4,5,1,2,2,1,5,2,4]
     },
 
     {
     firstName: "Patricia",
     lastName: "cass",
-    photo: "./images/patricia.jpg",
-    scores: [2,2,2,5,5,4,4,1,3,2]
+    //photo: "./images/patricia.jpg",
+    //scores: [2,2,2,5,5,4,4,1,3,2]
     },
 
     {
     firstName: "Olivia",
     lastName: "Ed",
-    photo: "./images/olivia.jpg",
-    scores: [3,3,3,3,5,2,1,3,2,4]
+    //photo: "./images/olivia.jpg",
+    //scores: [3,3,3,3,5,2,1,3,2,4]
     },
 
     {
     firstName: "Raul",
     lastName: "de blassio",
-    photo: "./images/raul.jpg",
-    scores: [4,4,4,1,2,1,5,4,2,3]
+    //photo: "./images/raul.jpg",
+    //scores: [4,4,4,1,2,1,5,4,2,3]
     }
 ];
 
