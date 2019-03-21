c5e571250f899b55c70d9bbf7ca6ec54713305f9
Radouane <38984550+Radouane@users.noreply.github.com>
2019-03-21 08:28:17 -0700

    sorting function

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -9,15 +9,31 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res) {
         friendsArray.push(req.body);
         res.json(true);
-        for (var i=0; i<friendsArray.length; i++){
-            var last = friendsArray.slice(-1)[0];
-            console.log([last.scores][0]);
-        }
 
+        for (var i=0; i<friendsArray.length; i++){
+            var lastFriend = friendsArray.slice(-1)[0];
+            console.log(lastFriend.scores);
+
+            var diffArr = [];
+            var sumArr = [];
+                for (var j=0; j<friendsArray[i].scores.length; j++){
+                    diffArr.push(Math.abs(parseInt(friendsArray[i].scores[j]) - parseInt(lastFriend.scores[j])));                
+                }        
+                console.log(diffArr);
+                
+                sumArr.push(diffArr.reduce((total, amount) => total + amount));
+                //console.log(friendSum);
+                //sumArr.push(friendSum);
+                console.log("insideloop: " + sumArr);
+                console.log("Inside Math.max: " + Math.max(sumArr));
+            }
+            console.log("outsideloop: " + sumArr);
+            console.log("Outside Math.max: " + Math.max(sumArr));
+            console.log("sumArr: " + sumArr);
+        // console.log(lastFriend.scores);
     });
 
     // Added to clear out array while working with the functionality.
-
     app.post("/api/clear", function(req, res) {
         friendsArray.length = [];
         res.json({ ok: true });
