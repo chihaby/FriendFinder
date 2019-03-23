f5d1403b8c352c56ad8478379cdd029f37a7de3e
Rad <chihaby@gmail.com>
2019-03-23 13:55:02 -0700

    express static added

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,44 +2,45 @@ var friendsArray = [{
 
     firstName: "Ed",
     lastName: "thompson",
-    photo: "FriendFinder\app\images\ed.jpg", 
-    scores: [2,4,1,3,2,4,5,1,2,2]
-    },
+    photo: "../images/ed.jpg",
+    scores: [2, 4, 1, 3, 2, 4, 5, 1, 2, 2]
+},
 
-    {
+{
     firstName: "Linda",
     lastName: "gerrard",
-    photo: "../images/linda.jpg",
-    scores: [2,4,3,5,1,2,3,2,4,1]
-    },
+    photo: "/images/patricia.jpg",
+    scores: [2, 4, 3, 5, 1, 2, 3, 2, 4, 1]
+},
 
-    {
+{
     firstName: "Eric",
     lastName: "baldwin",
-    photo: "../images/eric.jpg", 
-    scores: [3,4,5,1,2,2,1,5,2,4]
-    },
+    photo: "/images/eric.jpg",
+    scores: [3, 4, 5, 1, 2, 2, 1, 5, 2, 4]
+},
 
-    {
+{
     firstName: "Patricia",
     lastName: "cass",
-    photo: "../images/patricia.jpg",
-    scores: [2,2,2,5,5,4,4,1,3,2]
-    },
+    //photo: "https://vetstreet.brightspotcdn.com/dims4/default/cc1c07c/2147483647/crop/0x0%2B0%2B0/resize/180x180/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F45%2Fa06040b3ac11e092fe0050568d634f%2Ffile%2Fhub-dogs-adult.jpg",
+    photo: "/images/patricia.jpg",
+    scores: [2, 2, 2, 5, 5, 4, 4, 1, 3, 2]
+},
 
-    {
+{
     firstName: "Olivia",
     lastName: "Carr",
-    photo: "../images/olivia.jpg",
-    scores: [3,2,2,3,2,2,1,3,2,4]
-    },
+    photo: "/images/olivia.jpg",
+    scores: [3, 2, 2, 3, 2, 2, 1, 3, 2, 4]
+},
 
-    {
+{
     firstName: "Raul",
     lastName: "de blassio",
-    photo: "../images/raul.jpg",
-    scores: [2,3,4,1,2,1,5,4,2,3]
-    }
+    photo: "/images/raul.jpg",
+    scores: [2, 3, 4, 1, 2, 1, 5, 4, 2, 3]
+}
 ];
 
 module.exports = friendsArray;
--- app/server.js
+++ app/server.js
@@ -9,6 +9,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 
 // ================================================================================
@@ -25,6 +26,6 @@ require("./routes/htmlRoutes")(app);
 // The below code effectively "starts" our server
 // =============================================================================
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
