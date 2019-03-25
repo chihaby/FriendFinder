f83d0bcce3567f79f8f7abe5fdbfe69f0272b6cc
Radouane <38984550+Radouane@users.noreply.github.com>
2019-03-25 06:30:56 -0700

    completed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,42 +1,42 @@
 var friendsArray = [{
 
-    firstName: "Ed",
-    lastName: "thompson",
+    firstName: "Mark",
+    lastName: "Van Edden",
     photo: "../images/ed.jpg",
     scores: [2, 4, 1, 3, 2, 4, 5, 1, 2, 2]
 },
 
 {
-    firstName: "Linda",
-    lastName: "gerrard",
+    firstName: "Karla",
+    lastName: "Moris",
     photo: "/images/patricia.jpg",
     scores: [2, 4, 3, 5, 1, 2, 3, 2, 4, 1]
 },
 
 {
-    firstName: "Eric",
-    lastName: "baldwin",
+    firstName: "Tom",
+    lastName: "Howard",
     photo: "/images/eric.jpg",
     scores: [3, 4, 5, 1, 2, 2, 1, 5, 2, 4]
 },
 
 {
-    firstName: "Patricia",
-    lastName: "cass",
+    firstName: "Olivia",
+    lastName: "Carr",
     photo: "/images/patricia.jpg",
     scores: [2, 2, 2, 5, 5, 4, 4, 1, 3, 2]
 },
 
 {
-    firstName: "Olivia",
-    lastName: "Carr",
+    firstName: "Patricia ",
+    lastName: "Casas",
     photo: "/images/olivia.jpg",
     scores: [3, 2, 2, 3, 2, 2, 1, 3, 2, 4]
 },
 
 {
     firstName: "Raul",
-    lastName: "de blassio",
+    lastName: "De Blassio",
     photo: "/images/raul.jpg",
     scores: [2, 3, 4, 1, 2, 1, 5, 4, 2, 3]
 }
